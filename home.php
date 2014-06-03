42886158964a575166b15928f04f8c84b82be762
Aaron Prill <prilldev@gmail.com>
2014-06-03 11:12:34 -0400

    Removed unwanted code from home.php

--- home.php
+++ home.php
@@ -99,56 +99,6 @@ function locallife_home_loop_helper() {
 	
 }
 
-function locallife_home_loop_data() {
-
-    $arg = array(
-            /*'post_type' => 'guide', // this can be an array : array('guide','guide1',...)*/
-            'posts_per_page' => 5,
-            'order' => 'DESC',
-            'cat' => '-3,-4',
-            'post_status' => 'publish'
-            );
-    $query = new WP_Query($arg);
-
-    if ( $query->have_posts() ) : 
-        while ( $query->have_posts() ) : $query->the_post(); 
-        ?>
-            <div class="post">
-				<div class="entry-wrap">
-					<div class="entry-title">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-					</div>
-					<div class="entry-content">
-						<?php the_post_thumbnail('thumbnail'); ?>
-						<div class="entry-post-excerpt">
-							<?php the_excerpt(); ?>
-						</div>
-					</div>
-				</div>
-            </div>
-        <?php
-        endwhile;
-    endif;
-    wp_reset_query();
-}
-
-//* Add support for Genesis Grid Loop
-function locallife_child_grid_loop_helper() {
-  if ( function_exists( 'genesis_grid_loop' ) ) {
-		genesis_grid_loop( array(
-			'features' => 2,
-			'feature_image_size' => 0,
-			'feature_image_class' => 'alignleft post-image',
-			'feature_content_limit' => 0,
-			'grid_image_size' => 'grid-thumbnail',
-			'grid_image_class' => 'alignleft post-image',
-			'grid_content_limit' => 0,
-			'more' => __( '[Continue reading...]', 'genesis' ),
-		) );
-	} else {
-		genesis_standard_loop();
-	}
-}
 //* Remove the post meta function (use on frontpage only) */
 remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
  
